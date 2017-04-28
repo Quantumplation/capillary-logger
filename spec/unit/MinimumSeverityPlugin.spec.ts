802d0bb75afb0704cde0d4c94a70ae53a99c655d
Simon Gellis <simongellis@gmail.com>
2017-04-28 00:22:27 -0400

    [dev] Implement MinimumSeverityPlugin

---
+++ spec/unit/MinimumSeverityPlugin.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+
+import { MinimumSeverityPlugin } from '../../src/plugins/MinimumSeverityPlugin';
+
+describe('Minimum severity plugin', () => {
+  it('should allow messages above the minimum severity', () => {
+    const plugin = new MinimumSeverityPlugin('info');
+    const message = { message: 'o aint an int', severity: 'error' };
+    expect(plugin.process(message)).to.equal(message);
+  });
+
+  it('should allow messages at the minimum severity', () => {
+    const plugin = new MinimumSeverityPlugin('info');
+    const message = {
+      number: 47,
+      message: 'Cats use their whiskers to detect if they can fit through a space',
+      severity: 'info',
+    };
+    expect(plugin.process(message)).to.equal(message);
+  });
+
+  it('should reject messages below the minimum severity', () => {
+    const plugin = new MinimumSeverityPlugin('info');
+    const message = { message: 'this is fine', severity: 'trace' };
+    expect(plugin.process(message)).to.be.null;
+  });
+});
---
+++ src/plugins/MinimumSeverityPlugin.ts
@@ -0,0 +1,27 @@
+import { Severity } from '../logger';
+import { SyncPlugin } from '../plugin';
+
+const severityToRank = new Map<Severity, number>([
+  ['trace', 0],
+  ['debug', 1],
+  ['info',  2],
+  ['warn',  3],
+  ['error', 4],
+  ['fatal', 5],
+]);
+
+export class MinimumSeverityPlugin extends SyncPlugin {
+  private minimumRank: number;
+  constructor(minimum: Severity) {
+    super();
+    this.minimumRank = severityToRank.get(minimum) || 0;
+  }
+
+  public process(message: any) {
+    if (!message || (!message.severity && this.minimumRank)) {
+      return null;
+    }
+    const rank = severityToRank.get(message.severity) || 0;
+    return (rank >= this.minimumRank) ? message : null;
+  }
+}
